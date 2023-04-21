8c6235ac20130b8ff4a8bc39e7698786bbe70f5b
lucasvittal2 <lucasvittal@gmail.com>
2023-04-20 21:48:42 -0300

    Code and Viz review Done

--- Env/Constants.py
+++ Env/Constants.py
@@ -5,3 +5,6 @@ DS_JOBS = ["DATA SCIENTIST","DATA ENGINEER","DATA ANALYST","DATA ARCHITECT","BI 
 MY_COLORS = ['green','red','blue','purple','magenta','lime','cyan']
 
 JOB_COLORS  = ['blue', 'orange','green','red','purple','brown', 'magenta']
+
+#EXPIRIENCE_LEVEL_PALLETE = ['#c6fbc0','#9783aa','#7b58a1','#393f8c']
+EXPIRIENCE_LEVEL_PALLETE = ["#081d58", "#253494", "##225ea8", "#1d91c0"]
